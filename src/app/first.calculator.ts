b7dbeb92ddab181eec806ec53102494574f2c847
Jun Hu <hju@microsoft.com>
2023-11-03 08:55:35 -0500

    first test case more

--- src/app/first.calculator.ts
+++ src/app/first.calculator.ts
@@ -1,3 +1,10 @@
+import { Injectable } from "@angular/core";
+
+@Injectable(
+    {
+        providedIn: 'root'
+    }
+)
 export class Calculator {
     add(a: number, b: number) {
         return a + b;
--- src/app/first.test.spec.ts
+++ src/app/first.test.spec.ts
@@ -1,16 +1,22 @@
+import { TestBed } from "@angular/core/testing";
 import { Calculator } from "./first.calculator"
 
 describe('first test calculator', () => {
+
+    let calculator: Calculator;
+
+    beforeEach(() => {
+      TestBed.configureTestingModule({});
+      calculator = TestBed.inject(Calculator);
+    });
+
     it('should add 2 numbers', () => {
-        const calculator = new Calculator();
         expect(calculator.add(1, 2)).toBe(3);
     })
     it('should subtract 2 numbers', () => {
-        const calculator = new Calculator();
         expect(calculator.subtract(1, 2)).toBe(-1);
     })
     it('should multiply 2 numbers', () => {
-        const calculator = new Calculator();
         expect(calculator.multiply(1, 2)).toBe(2);
     })
 })
